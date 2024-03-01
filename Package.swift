bc0748a68d29c0e5462aa8a6a44201506d474a06
GitHub Action <action@github.com>
2024-03-01 11:25:41 +0000

    Update Package.swift for version 1.0.5

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
 //        .binaryTarget(name: "MeiliSDK", path: "../MeiliSDK/build/MeiliSDK.xcframework"),
-        .binaryTarget(name: "MeiliSDK", url: "https://github.com/hmarques98/meili-ios/releases/download/refs/heads/main/MeiliSDK-refs/heads/main.xcframework.zip")
+        .binaryTarget(name: "MeiliSDK", url: "https://github.com/hmarques98/meili-ios/releases/download/1.0.5/MeiliSDK-1.0.5.xcframework.zip")
     ]
 )
